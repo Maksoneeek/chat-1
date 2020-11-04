ee256a65533a186deb1007ebb39c4efde149b9da
unknown <denchikism789@gmail.com>
2020-11-04 21:55:12 +0300

    Add all types message view

--- src/components/Messages/MessagesItem.vue
+++ src/components/Messages/MessagesItem.vue
@@ -18,10 +18,12 @@
         <div
           class="content-chat-content__item"
           :class="{
-            'content-chat-content__item_big': message.type === 'image',
+            'content-chat-content__item_big': ['image', 'video'].includes(
+              message.type
+            ),
           }"
         >
-          <span v-if="message.text">
+          <span v-if="message.text && message.type !== 'file'">
             <span v-for="(text, index) in textArray" :key="index"
               >{{ text }}<br
             /></span>
@@ -31,6 +33,36 @@
             class="content-chat-content__img"
           >
             <img :src="message.url" alt="" />
+          </div>
+          <video v-else-if="message.type === 'video'" controls>
+            <source :src="message.url" type="video/mp4" />
+          </video>
+          <div
+            v-else-if="message.type === 'file'"
+            class="content-chat-content__file"
+          >
+            <a download :href="message.url">
+              <div class="content-chat-content__file_icon">
+                <img src="@/assets/img/file.png" alt="" />
+              </div>
+              <div class="content-chat-content__file_block">
+                <div class="content-chat-content__file_name">
+                  {{ message.text }}
+                </div>
+                <div class="content-chat-content__file_size">{{ size }}</div>
+              </div>
+            </a>
+          </div>
+          <div
+            v-else-if="message.type === 'audio'"
+            class="content-chat-content__audio"
+          >
+            <!-- <div class="content-chat-content__audio_icon">
+              <img src="@/assets/img/play.png" alt="" />
+            </div>
+            <div class="content-chat-content__audio_line"></div>
+            <div class="content-chat-content__item_time">00:20</div> -->
+            <audio :src="message.url" preload="auto" controls></audio>
           </div>
         </div>
       </div>
@@ -58,6 +90,18 @@ export default {
     },
     textArray() {
       return this.message.text.split("\n");
+    },
+    size() {
+      try {
+        let url = this.message.url;
+        let req = new XMLHttpRequest();
+        req.open("GET", url, false);
+        req.send();
+        return req.getResponseHeader("content-length") / 1024 + "кб";
+      } catch (e) {
+        console.log("Не удалось определить размер файла");
+      }
+      return "";
     },
   },
 };
